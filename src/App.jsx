8cc63e11895d78f7c8b6a226de81d8bb6288717e
JasubiPL <gameportal7@hotmail.com>
2023-02-01 12:28:51 -0600

    Se termina la Version 1.0.0 la cual solo contiene una pantalla para aprender las vocales

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import './App.css'
 import Vowels from './components/Vowels'
 
 const audioVocalA = document.getElementById("audioVocalA");
+const audioVocalE = document.getElementById("audioVocalE");
+const audioVocalI = document.getElementById("audioVocalI");
+const audioVocalO = document.getElementById("audioVocalO");
+const audioVocalU = document.getElementById("audioVocalU");
 
 function App() {
 
--- src/components/Vowels.jsx
+++ src/components/Vowels.jsx
@@ -6,13 +6,16 @@ import Letters from "./Letters";
 function Vowels(){
     return(
         <div className="centerLetters">
+            <div className='centerTittle'>
             <h1 className='lettersTittle'>Mi Escuelita Virtual</h1>
+            <p>Las Vocales</p>
+            </div>
             <div className='containerLetters'>
                 <Letters click={ () => audioVocalA.play()} >Aa</Letters>
-                <Letters>Ee</Letters>
-                <Letters>Ii</Letters>
-                <Letters>Oo</Letters>
-                <Letters>Uu</Letters>
+                <Letters click={ () => audioVocalE.play()} >Ee</Letters>
+                <Letters click={ () => audioVocalI.play()} >Ii</Letters>
+                <Letters click={ () => audioVocalO.play()} >Oo</Letters>
+                <Letters click={ () => audioVocalU.play()} >Uu</Letters>
             </div>
         </div>
     );
